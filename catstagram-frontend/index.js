7040b54e929dc46b7d5eaf1662d5338b4b70705e
github cjl248 <github cjl248@cornell.edu>
2019-09-11 18:25:11 -0400

    working again...better than before

--- catstagram-frontend/index.js
+++ catstagram-frontend/index.js
@@ -43,6 +43,96 @@ document.addEventListener('DOMContentLoaded', () => {
             })
         })
     }
+    
+    //BUTTON TO REVEAL NEW LOCATION FORM
+      const newLocationButton = document.createElement("button")
+      const newLocationButtonDiv = document.createElement("div")
+      newLocationButtonDiv.classList.add("new-location-div")
+
+      newLocationButton.innerText = "add dat new spot"
+      newLocationButton.classList.add("new-btn")
+      newLocationButtonDiv.appendChild(newLocationButton)
+
+      locationContainer.appendChild(newLocationButtonDiv)
+
+      //NEW LOCATION FORM
+      const newLocationForm = document.createElement("form")
+      const newLocationLabel = document.createElement("label")
+      newLocationLabel.innerText = "Add the details for your new location's below:"
+      newLocationForm.id = "new-location-form"
+      newLocationForm.hidden = true
+
+      const newLocationPicture = document.createElement("input")
+      newLocationPicture.placeholder = "picture link..."
+      const newLocationDescription = document.createElement("input")
+      newLocationDescription.placeholder = "description..."
+      const createLocationButton = document.createElement("button")
+      createLocationButton.innerText = "Add Location"
+      createLocationButton.id = "add-location-btn"
+
+      newLocationForm.appendChild(newLocationLabel)
+      newLocationForm.appendChild(newLocationPicture)
+      newLocationForm.appendChild(newLocationDescription)
+      newLocationForm.appendChild(createLocationButton)
+
+      newLocationButtonDiv.appendChild(newLocationForm)
+
+      locationContainer.addEventListener("click", (e) => {
+        // DELEGATION TO NEW LOCATION BUTTON
+        if (e.target.classList.contains("new-btn")) {
+          // change class on new location form to show
+          const newLocationForm = e.target.parentNode.childNodes[1]
+          const newLocationButton = e.target.parentNode.childNodes[0]
+          newLocationForm.hidden = false
+          newLocationForm.style.display = "flex"
+          newLocationButton.hidden = true
+        }
+        // DELEGATION TO ADD LOCATION BUTTON
+        if (e.target.id === "add-location-btn") {
+          e.preventDefault()
+          // console.log(e.target.parentNode.childNodes)
+          const pictureSrcValue = e.target.parentNode.childNodes[1].value
+          const descriptionValue = e.target.parentNode.childNodes[2].value
+          e.target.parentNode.reset()
+          console.log(pictureSrcValue);
+          console.log(descriptionValue);
+
+
+          // ADDING NEW LOCATION TO BACKEND
+          config = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              "description": descriptionValue,
+              "picture": pictureSrcValue,
+              "cat_id": catId,
+            })
+          }
+
+          fetch("http://localhost:3000/locations", config)
+          .then(resp => resp.json())
+          // .then(console.log)
+          .then((newLocation) => {
+            console.log(newLocation)
+            // ADDING NEW LOCATION TO DOM
+            locationContainer.innerHTML +=
+            `
+              <div data-id=${newLocation.id}>
+                <img src=${pictureSrcValue}/>
+                <p>${descriptionValue}</p>
+                <div class="reactions">
+                ${createButtonElement("😼", newLocation.reactions)}
+                ${createButtonElement("😻", newLocation.reactions)}
+                ${createButtonElement("💩", newLocation.reactions)}
+                ${createButtonElement("🙀", newLocation.reactions)}
+                </div>
+              </div>
+            `
+          })
+        }
+      })
     // !This handles creating a button element with the correct data
     function createButtonElement(catEmj,reactions) {
         // !This is setting up the filter for ALL the reactions on a location
@@ -53,9 +143,9 @@ document.addEventListener('DOMContentLoaded', () => {
         let results = reactions.filter((reaction) => { return reaction.cat_id === parseInt(catId) && reaction.emoji === catEmj })
 
         // !Returns the corresponding data for each button
-        return ` 
-        <button 
-        class="${results.length > 0 ? `done reaction-button` : `reaction-button`}" 
+        return `
+        <button
+        class="${results.length > 0 ? `done reaction-button` : `reaction-button`}"
         ${results.length > 0 ? `data-reaction-id=${results[0].id}` : ` `}>
 
             ${catEmj}<span data-count=${allEmojiReactionsCount}>${allEmojiReactionsCount}</span>
@@ -72,7 +162,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function updatingLocationForm(e){
         e.preventDefault()
-        
+
         locationBeingUpdated = parseInt(e.target.dataset.locationId)
         updatedDescription = e.target.description.value
         config = {
@@ -105,7 +195,7 @@ document.addEventListener('DOMContentLoaded', () => {
         loginForm.style.display = "none"
         mainContent.style.display = "block"
         welcomeMessage.innerText = `Hello Fellow Feline Friend ${catName}! 😼`
-        
+
         // !Calls the function to get all the locations
         getAllLocations()
     })
@@ -152,8 +242,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
             fetch('http://localhost:3000/reactions', config)
             .then(resp => resp.json())
-            .then(data => { 
-                e.target.dataset.reactionId = data.id 
+            .then(data => {
+                e.target.dataset.reactionId = data.id
             })
         }
 
@@ -181,7 +271,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     singleLocationContainer.querySelector('.location-description').innerHTML += `
                         <form id="edit-location-form" data-location-id="${location.id}">
                             <label>Add Your Changes</label>
-                            <input name="description" type="text" value="${location.description}"/> 
+                            <input name="description" type="text" value="${location.description}"/>
                             <button>Update Description</button>
                         </form>
                         <button class="edit-location-button">Edit</buton>`
