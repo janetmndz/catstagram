c47aef410f3ef44871e46b5a6094dceaeea81451
Janet Mendez <janetmendez.03@gmail.com>
2019-09-11 17:48:17 -0400

    fixed styles and update feature

--- catstagram-backend/db/seeds.rb
+++ catstagram-backend/db/seeds.rb
@@ -4,14 +4,14 @@ c3 = Cat.create(name: "Sox")
 c4 = Cat.create(name: "Mr. Pickles")
 c5 = Cat.create(name: "Garry")
 
-l1 = Location.create(description: "dat catnip on york by teh wework doe", cat: c2, picture: "https://placekitten.com/500/500")
-l2 = Location.create(description: "the old lady down by high street give free food if you meow x3...just don't scare the pidgeons away", cat: c1, picture: "https://placekitten.com/500/500")
-l3 = Location.create(description: "81 prospect rooftop: prime sunbathing location", cat: c3, picture: "https://placekitten.com/500/500")
-l4 = Location.create(description: "Dog sighting here! scurry awayyy", cat: c4, picture: "https://placekitten.com/500/500")
-l5 = Location.create(description: "Seafood market always gives out free fish if you meow nicely and wait at the door", cat: c1, picture: "https://placekitten.com/500/500")
-l6 = Location.create(description: "Prime bird watching spot. Or bird pouncing spot!", cat: c5, picture: "https://placekitten.com/500/500")
-l7 = Location.create(description: "Midnight acapell group's new locations! Come on by 2am to see the group!", cat: c2, picture: "https://placekitten.com/500/500")
-l8 = Location.create(description: "New cat in town! House cat!", cat: c5, picture: "https://placekitten.com/500/500")
+l1 = Location.create(description: "dat catnip on york by teh wework doe", cat: c2, picture: "https://i.imgur.com/q3pQENO.jpg")
+l2 = Location.create(description: "the old lady down by high street give free food if you meow x3...just don't scare the pidgeons away", cat: c1, picture: "https://i.imgur.com/YQ8Z2y7.jpg")
+l3 = Location.create(description: "81 prospect rooftop: prime sunbathing location", cat: c3, picture: "https://i.imgur.com/Afo0Y.jpg")
+l4 = Location.create(description: "Dog sighting here! scurry awayyy", cat: c4, picture: "https://i.imgur.com/Ttl1Hz2.jpg")
+l5 = Location.create(description: "Seafood market always gives out free fish if you meow nicely and wait at the door", cat: c1, picture: "https://i.imgur.com/yNyoB0v.jpg")
+l6 = Location.create(description: "Prime bird watching spot. Or bird pouncing spot!", cat: c5, picture: "https://i.imgur.com/Xmcoq71.jpg")
+l7 = Location.create(description: "Midnight acapell group's new locations! Come on by 2am to see the group!", cat: c2, picture: "https://i.imgur.com/qbKPZ.jpg")
+l8 = Location.create(description: "New cat in town! House cat!", cat: c5, picture: "https://i.imgur.com/2nqr7Jm.jpg")
 
 
 # Reactions: 💩, 😻, 🙀, 😼
